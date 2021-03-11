b22f1b5aac280249e3259f3e0e94da0dca19afe9
nadimnil <nadimmahmudmy@gmail.com>
2021-03-12 00:12:12 +0600

    final

--- src/components/LeagueDetails/LeagueDetails.js
+++ src/components/LeagueDetails/LeagueDetails.js
@@ -3,17 +3,13 @@ import './LeagueDetails.css';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab, faFacebook, faTwitterSquare, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faArrowRight, faBatteryThreeQuarters, faFlag, faFutbol, faGenderless, faInfo, faLandmark, faLowVision, faMale, faMap, faMars,faStore,faTwitter} from '@fortawesome/free-solid-svg-icons'
+import { faFlag, faFutbol,faLandmark,faMars,} from '@fortawesome/free-solid-svg-icons'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
